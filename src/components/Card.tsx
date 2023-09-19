ddfa4b086643065057451ed739a37b0c8ac517b0
Nath <playerxmatt@gmail.com>
2023-09-19 15:21:09 -0300

    style: added basic card styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,16 @@
 import { ReactNode } from "react";
+import classes from "./Card.module.css";
 
 interface CardProps {
   children: ReactNode;
+  shadow: boolean;
 }
 
 function Card(props: CardProps) {
+  const cardClasses = `${classes.card} ${props.shadow ? classes.shadow : ""}`;
+
   return (
-    <div>
+    <div className={cardClasses}>
       {props.children}
     </div>
   );
