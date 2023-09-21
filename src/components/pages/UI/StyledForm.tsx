ad2a575710c10e4e699eafa0174ad92f4b45dfc7
Nath <playerxmatt@gmail.com>
2023-09-21 16:31:07 -0300

    fix: sizing of elements

--- src/components/pages/UI/StyledForm.tsx
+++ src/components/pages/UI/StyledForm.tsx
@@ -1,44 +1,52 @@
 import styled from "styled-components";
 
 export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
   background-color: #fcfdff;
   border-radius: 16px;
   box-shadow: 0px 4px 4px #00000040;
-  left: 0;
-  position: fixed;
-  top: 0;
-  width: 20rem;
-  height: 36.375rem;
+  width: 24rem;
+  height: 37.375rem; 
 `;
 
+// Tem alguns titles que a fonte é Inter
 export const Title = styled.h1`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: #ed6d25;
-  font-family: Roboto;
-  font-size: 1.25rem;
   padding-top: 3.06rem;
   padding-bottom: 2.19rem;
+
+  color: #ed6d25;
+  font-family: "Roboto Flex";
+  font-size: 1.5rem;
   font-style: normal;
-  font-weight: 800;
-  line-height: normal;
+  font-weight: 600;
+  line-height: normal; 
+
+  &.inter {
+    font-family: "Inter";
+  }
 `;
 
 export const Input = styled.input`
+  width: 21rem;
+  height: 3.1875rem;
+
+  box-sizing: border-box;
+
   background: #eff3f8;
+
   border-radius: 8px;
   margin-bottom: 1.19rem;
+
+  font-size: 1rem;
+
+  padding: 0 1rem 0 1rem;
+
   &.small-input {
     width: 7.8125rem;
     height: 2.3125rem;
-    padding-left: 0.7rem;
-  }
-  &.special-input {
-    width: 281px;
-    height: 37px;
-    margin-left: 0.7rem;
-    padding-left: 1rem;
   }
 `;
 
@@ -53,22 +61,31 @@ export const DateInputContainer = styled.div`
 `;
 
 export const Button = styled.button`
-  width: 17.5625rem;
-  height: 2.3125rem;
+  width: 21rem;
+  height: 3.1875rem; 
   border-radius: 0.5rem;
   background: #ed6d25;
 
   color: #fff;
-  font-family: Roboto;
-  font-size: 0.875rem;
+  font-family: "Roboto Flex";
+  font-size: 1.125rem;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
 
+
+  &.secondary {
+    color: #ED6D25; 
+    background: #EFF3F8;
+  }
+`;
+
+export const Href = styled.a`
   display: flex;
   justify-content: center;
-  align-items: center;
-  margin-left: 1rem;
+  color: #ed6d25;
+  font-family: "Roboto Flex";
+  font-size: 1.125rem;
 `;
 
 export const Select = styled.select`
