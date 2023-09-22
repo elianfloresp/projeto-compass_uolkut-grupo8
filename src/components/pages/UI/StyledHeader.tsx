24569732824f38ecf72fc6f0130a7b421762d040
elianfloresp <elianfelipe.flores@gmail.com>
2023-09-22 12:02:11 -0300

    feat: header update

--- src/components/pages/UI/StyledHeader.tsx
+++ src/components/pages/UI/StyledHeader.tsx
@@ -1,15 +1,27 @@
 import styled from 'styled-components';
 
 const Header = styled.header`
-  width: 22.5rem;
-  height: 3.75rem;
-  background-color: #FFFFFF; 
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+  width: 88%;
+  height: 3.75rem;
+  background-color: #FFFFFF; 
+  margin: 0 auto; 
 `;
 
+
 const Title = styled.h1`
+  color: #ED6D25;
+  font-family: 'Roboto Flex', sans-serif;
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+`;
+
+
+const div = styled.img`
   color: #ED6D25;
   font-family: 'Roboto Flex', sans-serif;
   font-size: 1.25rem;
@@ -23,6 +35,10 @@ function header() {
   return (
     <Header>
       <Title>UOLkut</Title>
+      <div>
+      <img src="bg.png" alt="logo" />
+      <img src="arrow.png" alt="logo" />  
+      </div>
     </Header>
   );
 }
