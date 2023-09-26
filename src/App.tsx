326840d720807ad9070fa6503def8e72de46e8ec
Nath <playerxmatt@gmail.com>
2023-09-26 08:49:06 -0300

    fix: app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
 import React from "react";
 import Header from "./components/pages/UI/StyledHeader";
-import ProfileCard from "./components/Profile/ProfileCard";
-import NavigationBar from "./components/Profile/NavigationBar";
-import Editprofile from "./components/Profile/Editprofile";
-import FriendListCard from "./components/Profile/FriendListCard";
-import CommunityItem from "./components/Profile/CommunityItem";
-import CommunityListCard from "./components/Profile/CommunityListCard";
-
 import { Outlet } from "react-router-dom";
-import FriendItem from "./components/Profile/FriendItem";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <div className="navegation">
-        <NavigationBar />
-      </div>
-      <div className="profile">
-        <ProfileCard />
-      </div>
-      <div className="Editprofile">
-        <Editprofile />
-      </div>
-      <div className="Friendlistcard">
-      {/* <FriendListCard items={friendItems} /> */}
-      </div>
-     
+      <Outlet />
     </div>
   );
 };
