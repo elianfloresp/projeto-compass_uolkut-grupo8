746d7c8e9b20ea631d326d2bf978bc1aeee65619
Nath <playerxmatt@gmail.com>
2023-09-27 15:09:18 -0300

    fix: deleted console log on use media query hook

--- src/hooks/use-media-query.tsx
+++ src/hooks/use-media-query.tsx
@@ -5,7 +5,6 @@ const useMediaQuery = (query: string) => {
 
   useEffect(()=>{
     const media = window.matchMedia(query);
-    console.log(media);
 
     if (media.matches !== matches) setMatches(media.matches);
 
